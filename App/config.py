9ec2b1f1ed74cf0013960ff99b7bc141479435c6
Jhowmarcs <joaomarcosfir@gmail.com>
2025-04-24 22:15:33 -0300

    Adiciona arquivos iniciais: .gitignore, App, BD, Observabilidade e alterações no docker-compose.yml

--- App/config.py
+++ App/config.py
@@ -2,8 +2,5 @@ import os
 
 class Config:
     # Exemplo de URL para o banco PostgreSQL:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'postgresql://usuario:senha@db:5432/nome_banco'
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
